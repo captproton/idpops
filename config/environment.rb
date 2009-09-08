589f4de48bdfacdcbbf280420b98895fd7ea12e4
Carl Tanner <captproton@betterbilling.net>
2009-09-08 16:35:24 -0700

    initial commit of new round of development -- starting shoulda and factory_girl

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,9 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
   config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
+  config.gem 'webrat', :lib => "webrat", :source => "http://gems.github.com"
+  config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
+  
   
   
   # Only load the plugins named here, in the order given (default is alphabetical).
