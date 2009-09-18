e2eda962e529ae156040a51e6dea0d30c0267152
Carl Tanner <captproton@betterbilling.net>
2009-09-18 14:56:30 -0700

    took away invitation requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,8 @@
 class User < ActiveRecord::Base
   acts_as_authentic
   
-  validates_presence_of :invitation_id, :on => :create, :message => "is required"
-
   validates_uniqueness_of :username, :on => :create, :message => "must be unique"
-  has_many :sent_invitations, :class_name => "Invitation", :foreign_key => "sender_id"
-  belongs_to :invitation
   
-  before_create :set_invitation_limit
   
   attr_accessible :username, :email, :password, :password_confirmation, :invitation_token
 
