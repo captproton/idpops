857deb2691f2e659f626a4b38682bf4788ed0926
Carl Tanner <captproton@betterbilling.net>
2009-09-18 14:53:22 -0700

    loosened up routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ ActionController::Routing::Routes.draw do |map|
   map.login "/login", :controller => "user_sessions", :action => "new"
   map.logout "/logout", :controller => "user_sessions", :action => "destroy"
   map.register "/register", :controller => "users", :action => "create"
-  map.signup "/signup/:invitation_token", :controller => "users", :action => "new"
+  map.signup "/signup", :controller => "users", :action => "new"
   
 
   map.root :controller => 'frontdoor'
