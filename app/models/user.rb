8bb4c3f2dcd0379c18117db33b13728be447c4b2
Carl Tanner <captproton@betterbilling.net>
2009-09-18 14:58:07 -0700

    removed invitation from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,5 @@ class User < ActiveRecord::Base
   
   attr_accessible :username, :email, :password, :password_confirmation, :invitation_token
 
-  def invitation_token
-    invitation.token if invitation
-  end
-  
-  def invitation_token=(token)
-    self.invitation = Invitation.find_by_token(token)
-  end
-  
-  private
-  
-  def set_invitation_limit
-    self.invitation_limit = 25
-  end
 
 end
