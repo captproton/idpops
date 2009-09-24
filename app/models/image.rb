b77c34a69df59ce5a5855d8383f36e0a6b37dc1f
Carl Tanner <captproton@betterbilling.net>
2009-09-24 16:36:45 -0700

    continuation of attachment of paperclip assets

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,12 +2,12 @@ class Image < ActiveRecord::Base
   belongs_to :idea
 
   has_attached_file :graphic,
-  		:storage => ENV['S3_BUCKET'] ? :s3 : :filesystem,
+  		:storage => ENV['s3_bucket_name'] ? :s3 : :filesystem,
   		:s3_credentials => {
-  			:access_key_id => ENV['S3_KEY'],
-  			:secret_access_key => ENV['S3_SECRET']
+  			:access_key_id => ENV['s3_access_id'],
+  			:secret_access_key => ENV['s3_secret_key']
   		},
-  		:bucket => ENV['S3_BUCKET'],
+  		:bucket => ENV['s3_bucket_name'],
   		:path => ":class/:id_partition/:basename.:extension",
   		:s3_permissions => "private"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090924213808) do
+ActiveRecord::Schema.define(:version => 20090924233551) do
 
   create_table "comments", :force => true do |t|
     t.integer  "believer_id"
@@ -50,6 +50,9 @@ ActiveRecord::Schema.define(:version => 20090924213808) do
     t.integer  "imaginer_id"
     t.text     "blurb"
     t.datetime "deadline"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
   end
 
   create_table "images", :force => true do |t|
