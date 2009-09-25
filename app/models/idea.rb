7c8dfe7de785533ce555429e9d8851ff71b2692e
Carl Tanner <captproton@betterbilling.net>
2009-09-24 17:23:38 -0700

    changed styles for idea paperclip

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -5,7 +5,7 @@ class Idea < ActiveRecord::Base
     
   if CONFIG['s3']
     has_attached_file :photo, :storage => :s3,
-                      :styles  => { :large_thumb => "200x150>", :original => "800x600>"},
+                      :styles  => { :large_thumb => "200x150>", :big => "800x600>"},
                       :bucket => CONFIG['s3_bucket_name'], # TODO is there a way to share this between models?
                       :s3_credentials => { :access_key_id => CONFIG['s3_access_id'], :secret_access_key => CONFIG['s3_secret_key'] },
                       :s3_headers => { 'Cache-Control' => 'max-age=315576000', 'Expires' => 10.years.from_now.httpdate }
